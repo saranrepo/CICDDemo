17aa7b1050293211568dfaaa8d513d97b36df7d8
SARAN <SARAN@192.168.1.10>
2019-02-02 11:28:02 +0530

    changes made

--- CICDUsingGit/src/FirstTest.java
+++ CICDUsingGit/src/FirstTest.java
@@ -3,6 +3,8 @@ public class FirstTest {
 
 	public static void main(String[] args) {
 		System.out.println("eclipse integrated with repository");
+		System.out.println("made some changes");
+		System.out.println("Running through jenkins poll scm");
 	}
 
 }
