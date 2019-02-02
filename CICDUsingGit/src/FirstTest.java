e13409f6ee898a996d598793dd2f5f6341817e07
SARAN <SARAN@192.168.1.10>
2019-02-02 11:32:36 +0530

    CHANGES FOR POLL SCM

--- CICDUsingGit/src/FirstTest.java
+++ CICDUsingGit/src/FirstTest.java
@@ -4,7 +4,7 @@ public class FirstTest {
 	public static void main(String[] args) {
 		System.out.println("eclipse integrated with repository");
 		System.out.println("made some changes");
-		System.out.println("Running through jenkins poll scm");
+		System.out.println("Running through jenkins POLL SCM");
 	}
 
 }
